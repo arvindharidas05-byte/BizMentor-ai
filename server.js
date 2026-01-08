76a97ea0f4258a16c6efd8179d2bb3809a91fa5b
arvindharidas05-byte <arvindharidas05@gmail.com>
2026-01-08 23:37:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 /* ---------------- AI ENDPOINT ---------------- */
-app.post("/ai", async (req, res) => {
+app.post("", async (req, res) => {
   const { idea } = req.body;
 
   const prompt = `
@@ -65,4 +65,5 @@ app.post("/api/create-order", async (req, res) => {
 app.listen(5000, () => {
   console.log("BizMentor AI backend running on port 5000");
 });
+
 
